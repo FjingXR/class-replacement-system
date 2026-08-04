110f753904a4ed9585d8a605a94a6eca3efbdef2
FjingXR <poongfj-sm23@student.tarc.edu.my>
2026-08-04 19:03:20 +0800

    ui: minor fixes across timetable templates, mock-data, and sdd prompt

--- resources/views/ui-design-templates/student-my-timetable-UI-design-template.blade.php
+++ resources/views/ui-design-templates/student-my-timetable-UI-design-template.blade.php
@@ -134,7 +134,7 @@
                         abbr: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'][d],
                         date: fmt(dt),
                         sunday: d === 6,
-                        today: (function() { var t = new Date('2026-09-03'); t.setHours(0,0,0,0); return dt.getTime() === t.getTime(); })(),
+                        today: (function() { var t = new Date('2026-08-02'); t.setHours(0,0,0,0); return dt.getTime() === t.getTime(); })(),
                         holiday: holiday,
                         holidayLabel: holidayLabel,
                     });
@@ -190,7 +190,7 @@
 
         function currentWeekIndex() {
             const semesterStart = new Date(MockData.semester.startDate);
-            const today = new Date('2026-09-03');
+            const today = new Date('2026-08-02');
             today.setHours(0, 0, 0, 0);
             const idx = Math.floor((today - semesterStart) / 86400000 / 7);
             return Math.max(0, Math.min(weekData.length - 1, idx));
