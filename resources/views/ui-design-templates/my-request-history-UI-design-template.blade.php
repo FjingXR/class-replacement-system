113dbada7b23f8bddb5a9be6b86cb4e1e34caf04
FjingXR <poongfj-sm23@student.tarc.edu.my>
2026-08-07 10:06:39 +0800

    fix: add table-layout fixed + explicit widths for all columns

--- resources/views/ui-design-templates/my-request-history-UI-design-template.blade.php
+++ resources/views/ui-design-templates/my-request-history-UI-design-template.blade.php
@@ -71,15 +71,18 @@
         }
 
         /* ───── Column Widths ───── */
-        .timetable td.col-requested-at { width: 140px; white-space: normal; }
+        .col-checkbox { width: 40px; }
+        .timetable th.col-requested-at, .timetable td.col-requested-at { width: 125px; white-space: normal; }
+        .col-code { width: 180px; }
         .col-original { width: 170px; }
         .col-replacement { width: 170px; }
         .col-original, .col-replacement { white-space: normal; }
         .col-venue { width: 75px; }
+        .col-students { width: 70px; }
         .col-cohort { width: 120px; }
         .col-status { width: 130px; }
 
-        .grid-scroll .timetable { min-width: 1195px; }
+        .grid-scroll .timetable { min-width: 1150px; table-layout: fixed; }
 
         .col-replacement .cell-class-block .class-time {
             font-weight: 600;
