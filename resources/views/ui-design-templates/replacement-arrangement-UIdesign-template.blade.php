83f2c0e4e48d82000c9f4c0a85af701b6cab1ba7
FjingXR <poongfj-sm23@student.tarc.edu.my>
2026-08-09 22:05:36 +0800

    fix: remove leftover min-height on cell-content so it fills 80px hour-cell

--- resources/views/ui-design-templates/replacement-arrangement-UIdesign-template.blade.php
+++ resources/views/ui-design-templates/replacement-arrangement-UIdesign-template.blade.php
@@ -286,7 +286,6 @@
         .cell-content {
             width: 100%;
             height: 100%;
-            min-height: 52px;
             display: flex;
             align-items: center;
             justify-content: center;
