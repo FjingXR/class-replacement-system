0674f6904534491ff6b1e1ca093cd2684ad1813e
FjingXR <poongfj-sm23@student.tarc.edu.my>
2026-08-15 12:10:11 +0800

    ui: theme-toggle hover tooltip on the left side

--- resources/views/partials/ui-nav-bar.blade.php
+++ resources/views/partials/ui-nav-bar.blade.php
@@ -21,7 +21,7 @@
     </div>
 
     <div class="top-right">
-        <button class="theme-toggle" onclick="toggleTheme()" aria-label="Toggle theme">
+        <button class="theme-toggle" onclick="toggleTheme()" aria-label="Toggle theme" data-tip="Switch between light &amp; dark mode" data-tip-pos="left">
             <svg id="theme-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
             </svg>
